aa3094dd10966b3fce0b56543dc850c69ae0221c
R-Sliw <robert.sliwinski90@gmail.com>
2022-07-28 19:59:39 +0200

    added lesson 11 video

---
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -0,0 +1,70 @@
+/* Get Our Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+/* Build out functions */
+
+function togglePlayer() {
+  const method = video.paused ? "play" : "pause";
+
+  video[method]();
+  /*if (video.paused)
+	{
+		video.play();
+	} else {video.pause()}*/
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  //const skok = button.date
+  video.currentTime = video.currentTime + parseFloat(this.dataset.skip);
+}
+
+function hendelRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.name);
+  console.log(this.value);
+}
+
+function timeBarChange() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function barPlace(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+
+  video.currentTime = scrubTime;
+}
+
+/* Hook up the event listeners */
+
+video.addEventListener("click", togglePlayer);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", timeBarChange);
+
+toggle.addEventListener("click", togglePlayer);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", hendelRangeUpdate));
+
+progress.addEventListener("click", barPlace);
+
+let puchdown = false;
+
+progress.addEventListener("mousemove", (e) => puchdown && barPlace(e));
+progress.addEventListener("mousedown", () => (puchdown = true));
+progress.addEventListener("mouseup", () => (puchdown = false));
